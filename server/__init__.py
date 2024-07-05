ce4f8f5a8dd9afb30f430c9ce2e468720a53408b
MustafaJafar <mustafataherzaky@outlook.com>
2024-07-05 11:56:23 +0300

    remove redundant code

--- server/__init__.py
+++ server/__init__.py
@@ -3,13 +3,9 @@ from typing import Type
 from ayon_server.addons import BaseServerAddon
 
 from .settings import ColorbleedSettings, DEFAULT_VALUES
-from .version import __version__
 
 
 class ColorbleedAddon(BaseServerAddon):
-    name = "colorbleed"
-    title = "Colorbleed"
-    version = __version__
     settings_model: Type[ColorbleedSettings] = ColorbleedSettings
 
     async def get_default_settings(self):
