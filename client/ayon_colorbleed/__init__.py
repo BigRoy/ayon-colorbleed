5ec88cbad698737cb4fb7a20763c48c215b8c570
Roy Nieterau <roy_nieterau@hotmail.com>
2024-09-24 02:30:09 +0200

    Draft to implement web actions to play a publish using desktop app or show a publish in explorer.

--- client/ayon_colorbleed/__init__.py
+++ client/ayon_colorbleed/__init__.py
@@ -1,9 +1,42 @@
 import os
+import platform
+import subprocess
 
-from ayon_core.addon import AYONAddon
+import ayon_api
+from ayon_core.addon import AYONAddon, click_wrap, ensure_addons_are_process_ready
 from ayon_core.addon.interfaces import IPluginPaths
 
+
 from .version import __version__
+
+
+def _resolve_entity_uris(uris):
+    """Resolve AYON Entity URIs to filepath
+
+    Arguments:
+        uris (list[str]): The AYON entity URIs.
+
+    Returns:
+        list[str]: The filepaths
+
+    """
+    response = ayon_api.post("resolve", resolveRoots=True, uris=uris)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Unable to resolve AYON entity URI: '{uris}'"
+        )
+
+    paths = []
+    for entity in response.data["entities"]:
+        paths.append(entity["filePath"])
+    return paths
+
+
+def resolve_entity_uri(entity_uri):
+    if not entity_uri.startswith(("ayon://", "ayon+entity://")):
+        # Assume not an entity URI
+        return entity_uri
+    return _resolve_entity_uris([entity_uri])
 
 
 class ColorbleedAddon(AYONAddon, IPluginPaths):
@@ -21,3 +54,79 @@ class ColorbleedAddon(AYONAddon, IPluginPaths):
             "load": [os.path.join(plugins_dir, "load")],
             "publish": [os.path.join(plugins_dir, "publish")]
         }
+
+    # region CLI
+    def cli(self, addon_click_group):
+        main_group = click_wrap.group(
+            self._cli_main, name=self.name, help="Colorbleed addon"
+        )
+        (
+            main_group.command(
+                self._cli_run,
+                name="run",
+                help="Run filepaths"
+            )
+            .argument("paths", nargs=-1, required=True)
+        )
+        (
+            main_group.command(
+                self._cli_show_in_explorer,
+                name="show-in-explorer",
+                help="Show given paths or entity URIs in explorer."
+            )
+            .argument("paths", nargs=-1, required=True)
+        )
+        # Convert main command to click object and add it to parent group
+        addon_click_group.add_command(
+            main_group.to_click_obj()
+        )
+
+    def _cli_main(self):
+
+        ensure_addons_are_process_ready(
+            addon_name=self.name,
+            addon_version=self.version
+        )
+
+    def _cli_run(
+        self, paths
+    ):
+        """Run paths using OS default application"""
+        print(f"Running: {paths}")
+        for path in paths:
+            path = resolve_entity_uri(path)
+            self.run_file(path)
+
+    def _cli_show_in_explorer(self, paths):
+        """Open paths in system explorer"""
+        print(f"Running show in explorer: {paths}")
+        for path in paths:
+            path = resolve_entity_uri(path)
+            self.open_in_explorer(os.path.dirname(path))
+    # endregion
+
+    @staticmethod
+    def run_file(path):
+        platform_name = platform.system().lower()
+        if platform_name == 'windows':  # Windows
+            os.startfile(path)
+        elif platform_name == 'darwin':  # macOS
+            subprocess.Popen(('open', path))
+        elif platform_name == "linux":  # linux variants
+            subprocess.Popen(('xdg-open', path))
+        else:
+            raise RuntimeError(f"Unknown platform {platform.system()}")
+
+    @staticmethod
+    def open_in_explorer(path: str):
+        platform_name = platform.system().lower()
+        if platform_name == "windows":
+            args = ["start", path]
+        elif platform_name == "darwin":
+            args = ["open", "-na", path]
+        elif platform_name == "linux":
+            args = ["xdg-open", path]
+        else:
+            raise RuntimeError(f"Unknown platform {platform.system()}")
+        # Make sure path is converted correctly for 'os.system'
+        os.system(subprocess.list2cmdline(args))
--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,13 @@
-from typing import Type
+from typing import Type, TYPE_CHECKING
 
+from nxtools import logging
+from ayon_server.actions import SimpleActionManifest
 from ayon_server.addons import BaseServerAddon
 
 from .settings import ColorbleedSettings, DEFAULT_VALUES
+
+if TYPE_CHECKING:
+    from ayon_server.actions import ActionExecutor, ExecuteResponseModel
 
 
 class ColorbleedAddon(BaseServerAddon):
@@ -11,3 +16,74 @@ class ColorbleedAddon(BaseServerAddon):
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
         return settings_model_cls(**DEFAULT_VALUES)
+
+    async def get_simple_actions(
+        self,
+        project_name: str | None = None,
+        variant: str = "production",
+    ) -> list[SimpleActionManifest]:
+        return [
+            SimpleActionManifest(
+                identifier="colorbleed.open_file",
+                label="Open file",
+                category="Colorbleed",
+                order=100,
+                icon={
+                    "type": "material-symbols",
+                    "name": "live_tv",
+                    "color": "#FFA500"
+                },
+                entity_type="version",
+                entity_subtypes=None,
+                allow_multiselection=False,
+            ),
+            SimpleActionManifest(
+                identifier="colorbleed.show_in_explorer",
+                label="Show in explorer",
+                category="Colorbleed",
+                order=100,
+                icon={
+                    "type": "material-symbols",
+                    "name": "folder_open",
+                    "color": "#FFA500"
+                },
+                entity_type="version",
+                entity_subtypes=None,
+                allow_multiselection=False,
+            )
+        ]
+
+    async def execute_action(
+        self,
+        executor: "ActionExecutor",
+    ) -> "ExecuteResponseModel":
+        """Execute an action provided by the addon"""
+        logging.debug(f"Executing ayon-colorbleed action: {executor.context}")
+        # context = executor.context
+        # project_name = context.project_name
+        # representation_ids = list()
+        # for entity_subtype, entity_id in zip(context.entity_subtypes,
+        #                                      context.entity_ids):
+        #     if entity_subtype == "representation":
+        #         representation_ids.append(entity_id)
+        # if not representation_ids:
+        #     return await executor.get_server_action_response(
+        #         success=True,
+        #         message="No representations"
+        #     )
+
+        # TODO: Get the AYON entity URIs instead so the client running
+        #  can resolve the entity's filepath
+        filepath = r'E:/test.png'
+        if executor.identifier == "colorbleed.show_in_explorer":
+            return await executor.get_launcher_action_response(
+                args=[
+                    "addon", "colorbleed", "show-in-explorer", filepath
+                ]
+            )
+        elif executor.identifier == "colorbleed.open_file":
+            return await executor.get_launcher_action_response(
+                args=[
+                    "addon", "colorbleed", "run", filepath
+                ]
+            )
